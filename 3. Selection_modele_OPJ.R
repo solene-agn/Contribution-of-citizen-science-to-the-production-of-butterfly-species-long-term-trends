34129c102d2746afaf87c116a8ea16f27a1fd6b3
Solène Agnoux <76123468+solene-agn@users.noreply.github.com>
2026-01-22 15:35:41 +0100

    Update 3. Selection_modele_OPJ.R

--- 3. Selection_modele_OPJ.R
+++ 3. Selection_modele_OPJ.R
@@ -1,16 +1,20 @@
 library("dplyr")
 
+# We download model outputs
+
+# Poisson
 glmm.TMB <- read.csv2("data/OPJ/TMB_TT/TMB/opj_month.TMB.csv", sep=";")
 glmm.TMB[c("Estimate_scaled","StdErr_scaled", "Estimate", "SdErrVariable", "Chisq", "Df", "pval", "Overdisp", "OverdispBolker","ovd.DHARMa", "OverdispDHARMa", "AIC","BIC", "zero_infl", "zero_infl.pv", "KS","KS.pv",  "Outlier.pv", "NbPresence", "Total")] <- sapply(glmm.TMB[c("Estimate_scaled","StdErr_scaled", "Estimate", "SdErrVariable", "Chisq", "Df", "pval", "Overdisp", "OverdispBolker","ovd.DHARMa", "OverdispDHARMa", "AIC","BIC", "zero_infl", "zero_infl.pv", "KS","KS.pv",  "Outlier.pv", "NbPresence", "Total")], as.numeric)
-glmm.TMB <- glmm.TMB %>% mutate_if(is.numeric,~round(.,3)) # Arrondir les valeurs des colonnes numeriques
+glmm.TMB <- glmm.TMB %>% mutate_if(is.numeric,~round(.,3))
 glmm.TMB <- glmm.TMB[(glmm.TMB$Effect=="YEAR"),]
 names(glmm.TMB)[names(glmm.TMB)=="Esp"] <- "GROUP"
 glmm.TMB[is.na(glmm.TMB)] <- "NA"
 glmm.TMB$AIC <- as.numeric(as.character(glmm.TMB$AIC))
 
+# Negative binomial
 glmm.TMB.nb <- read.csv2("data/OPJ/TMB_TT/TMB_nb/opj_month.TMB.NB.csv", sep=";")
 glmm.TMB.nb[c("Estimate_scaled","StdErr_scaled", "Estimate", "SdErrVariable", "Chisq", "Df", "pval", "Overdisp", "OverdispBolker","ovd.DHARMa", "OverdispDHARMa", "AIC","BIC", "zero_infl", "zero_infl.pv", "KS","KS.pv",  "Outlier.pv", "NbPresence", "Total")] <- sapply(glmm.TMB.nb[c("Estimate_scaled","StdErr_scaled", "Estimate", "SdErrVariable", "Chisq", "Df", "pval", "Overdisp", "OverdispBolker","ovd.DHARMa", "OverdispDHARMa", "AIC","BIC", "zero_infl", "zero_infl.pv", "KS","KS.pv",  "Outlier.pv", "NbPresence", "Total")], as.numeric)
-glmm.TMB.nb <- glmm.TMB.nb %>% mutate_if(is.numeric,~round(.,3)) # Arrondir les valeurs des colonnes numeriques
+glmm.TMB.nb <- glmm.TMB.nb %>% mutate_if(is.numeric,~round(.,3))
 glmm.TMB.nb <- glmm.TMB.nb[(glmm.TMB.nb$Effect=="YEAR"),]
 names(glmm.TMB.nb)[names(glmm.TMB.nb)=="Esp"] <- "GROUP"
 glmm.TMB.nb[is.na(glmm.TMB.nb)] <- "NA"
@@ -45,7 +49,7 @@ for (e in unique(glmm.TMB.nb$GROUP)){
                                                          pval=glmm.TMB.nb$pval[glmm.TMB.nb$GROUP==e],
                                                          # OverdispBolker.ratio=glmm.TMB.nb$OverdispBolker.ratio[glmm.TMB.nb$GROUP==e],
                                                          # OverdispBolker.pv=glmm.TMB.nb$OverdispBolker.pv[glmm.TMB.nb$GROUP==e],
-                                                         DeltaAIC=glmm.TMB$AIC[glmm.TMB$GROUP==e]-glmm.TMB.nb$AIC[glmm.TMB.nb$GROUP==e],
+                                                         DeltaAIC=glmm.TMB.nb$AIC[glmm.TMB$GROUP==e]-glmm.TMB.nb$AIC[glmm.TMB.nb$GROUP==e],
                                                          NbPresence=glmm.TMB.nb$NbPresence[glmm.TMB.nb$GROUP==e],
                                                          Total=glmm.TMB.nb$Total[glmm.TMB.nb$GROUP==e])
   )}
@@ -62,3 +66,4 @@ missing <- missing[,c("GROUP", "Modele", "Estimate_scaled", "StdErr_scaled", "Es
 Selection_opj <- rbind(Selection_opj, missing)
 
 write.table(Selection_opj, "data/OPJ/TMB_TT/Trend_opj.csv", sep=";", row.names=FALSE)
+
